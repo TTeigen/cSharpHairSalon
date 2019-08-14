0f82df5b22ddb72bc879b7cb557f617b2503e3d4
Student <example@example.com>
2019-08-14 16:38:46 -0700

    fix Entity error

---
+++ HairSalon/Models/Client.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public int StylistId { get; set; }
+        public int ClientId { get; set; }
+    }
+}
---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,11 @@
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        
+        public string Name { get; set; }
+        public string Specialty { get; set; }
+        public int StylistId { get; set; }
+    }
+}
